7143f10fba6069f056ae86eb1d5dd486f81e4a42
fahad-israr <fahad00cms@gmail.com>
2020-05-09 16:49:05 +0530

    @Args command lined

--- src/main/java/org/acme/getting/started/HelloWorldMain.java
+++ src/main/java/org/acme/getting/started/HelloWorldMain.java
@@ -1,13 +1,13 @@
 package org.acme.getting.started;
 
 import io.quarkus.runtime.QuarkusApplication;
-import io.quarkus.runtime.annotations.QuarkusMain;
 
 
 public class HelloWorldMain implements QuarkusApplication {
   @Override
   public int run(String... args) throws Exception {   
     System.out.println("Hello World");
+    System.out.println(args[0]);
     return 10;
  }
 }
